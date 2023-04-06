b92384ea7dcd6ff3e5a66ea5548b2786e9823802
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-06 19:53:16 +0100

    Compound Assignment With Augmented Addition

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/augmentedAddition.js
@@ -0,0 +1,12 @@
+// Compound Assignment With Augmented Addition
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a = a += 12;
+b = b += 9;
+c = c += 7;
+
+console.log(a,b,c)
