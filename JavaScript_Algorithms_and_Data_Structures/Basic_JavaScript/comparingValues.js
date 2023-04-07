22b2539f7b4f65ac544fdcb12696bc53b9ba9b48
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-07 16:17:31 +0100

    Practice comparing different values

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/comparingValues.js
@@ -0,0 +1,11 @@
+// Practice comparing different values
+
+// Setup
+function compareEquality(a, b) {
+    if (a === b) { // Change this line If we use a == b return true (Equal)
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+console.log(compareEquality(10, "10"));
