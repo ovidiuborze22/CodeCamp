4f46fd19ff2a292ff52f063b121bb7987a9ea525
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-07 13:55:21 +0100

    Manipulate Arrays With pop Method

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/popMethod.js
@@ -0,0 +1,10 @@
+// Manipulate Arrays With pop Method
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.pop()
+
+console.log(removedFromMyArray)
+console.log(myArray)
