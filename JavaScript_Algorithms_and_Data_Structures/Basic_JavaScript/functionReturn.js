466931979dff407bdb652e81d1690133f1769a0e
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-07 14:44:17 +0100

    Return a Value from a Function with Return

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/functionReturn.js
@@ -0,0 +1,9 @@
+// Return a Value from a Function with Return
+
+function timesFive(num) {
+  return num * 5;
+}
+
+console.log(timesFive(5));
+console.log(timesFive(10));
+console.log(timesFive(0));
