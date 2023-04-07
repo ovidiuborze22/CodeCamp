c6abc309d1bc35e9c7c779fcf095c197bd4d6ae5
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-07 19:43:18 +0100

    Logical Order in If Else Statements

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/orderIfElse.js
@@ -0,0 +1,15 @@
+// Logical Order in If Else Statements
+
+function orderMyLogic(val) {
+    if (val < 5) {
+      return "Less than 5";
+    } else if (val < 10) {
+      return "Less than 10";
+    } else {
+      return "Greater than or equal to 10";
+    }
+  }
+  
+console.log(orderMyLogic(4));
+console.log(orderMyLogic(6));
+console.log(orderMyLogic(11));
