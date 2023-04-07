54ad6e455d7da4ccb89ecc99fe806383de218021
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-07 19:11:47 +0100

    Comparison with the Less Than Operator

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/lessThanOperator.js
@@ -0,0 +1,20 @@
+// Comparison with the Less Than Operator
+
+function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+console.log(testLessThan(0));
+console.log(testLessThan(24));
+console.log(testLessThan(25));
+console.log(testLessThan(54));
+console.log(testLessThan(55));
+console.log(testLessThan(99));
