3b5cdbc68029cc44b80ca27e778c3921f7b5a3ff
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-09 15:25:44 +0100

    Build JavaScript Objects

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/objects.js
@@ -0,0 +1,13 @@
+// Build JavaScript Objects
+
+const myDog = {
+    // Only change code below this line
+    "name": "Boby",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Rexi", "Dolca"]
+    // Only change code above this line
+};
+
+console.log(myDog.name)
+console.log(myDog.friends)
