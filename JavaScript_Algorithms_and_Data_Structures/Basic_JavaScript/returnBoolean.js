4be2424efeaf811cba80470b2113e260e0298756
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-09 14:49:23 +0100

    Returning Boolean Values from Functions

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/returnBoolean.js
@@ -0,0 +1,10 @@
+// Returning Boolean Values from Functions
+
+function isLess(a, b) {
+    // Only change code below this line
+    return a <= b;
+    // Only change code above this line
+  }
+  
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
