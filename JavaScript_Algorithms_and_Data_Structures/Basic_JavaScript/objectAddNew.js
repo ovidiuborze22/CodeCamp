ff14383d0393a2b49aeabb822d5cb84f2f5f59aa
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-09 15:51:13 +0100

    Add New Properties to a JavaScript Object

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/objectAddNew.js
@@ -0,0 +1,13 @@
+// Add New Properties to a JavaScript Object
+
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"]
+};
+  
+myDog.bark = "woof" // or myDog["bark"] = "woof";
+
+console.log(myDog)
+console.log(myDog.bark)
