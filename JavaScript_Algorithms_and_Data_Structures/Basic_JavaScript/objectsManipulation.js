19689b08bb831baca3b0216b7e4ff0c28cd40964
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-10 11:34:14 +0100

    Manipulating Complex Objects

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/objectsManipulation.js
@@ -0,0 +1,29 @@
+// Manipulating Complex Objects
+
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+    {
+      "artist": "Inna",
+      "title": "I love You",
+      "release_year": 2010,
+      "formats": [
+        "CD",
+        "8T",
+        "LP",
+        "DVD"
+      ],
+      "gold": false 
+    }
+];
+
+console.log(myMusic[0].formats);
