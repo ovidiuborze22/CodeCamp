4b98673d490b32fb549d34eb54c8e46e2b3ccce8
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-10 16:50:32 +0100

    Iterate with JavaScript Do...While Loops

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/iterateDoWhileLoops.js
@@ -0,0 +1,17 @@
+// Iterate with JavaScript Do...While Loops
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+do {
+  myArray.push(i);
+  i++;
+} while (i <= 10);
+
+
+
+
+console.log(myArray)
+console.log(i)
