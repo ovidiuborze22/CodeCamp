f30954420b8311e6b8a74753197ce4ecf7fa4ea2
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-10 12:02:47 +0100

    Accessing Nested Objects

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/objectsNested.js
@@ -0,0 +1,17 @@
+// Accessing Nested Objects
+
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+const gloveBoxContents = myStorage.car.inside["glove box"];
+
+console.log(gloveBoxContents)
