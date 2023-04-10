62f01f8ddfcb0b5df776f9fb92b49b7ef69b99b5
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-10 16:39:20 +0100

    Nesting For Loops

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/nestingForLoops.js
@@ -0,0 +1,17 @@
+// Nesting For Loops
+
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+    for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+    // Only change code above this line
+    return product;
+  }
+  
+console.log(multiplyAll([[1], [2], [3]]));
+console.log(multiplyAll([[1, 2], [3, 4], [5, 6, 7]]));
+console.log(multiplyAll([[5, 1], [0.2, 4, 0.5], [3, 9]]));
