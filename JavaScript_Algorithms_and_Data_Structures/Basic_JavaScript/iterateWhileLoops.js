ced1f987f9c237e7e458aa58e508296d58039be8
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-10 12:59:56 +0100

    Iterate with JavaScript While Loops

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/iterateWhileLoops.js
@@ -0,0 +1,13 @@
+// Iterate with JavaScript While Loops
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+let i = 5;
+while(i >= 0) {
+  myArray.push(i);
+  i--;
+}
+
+console.log(myArray)
