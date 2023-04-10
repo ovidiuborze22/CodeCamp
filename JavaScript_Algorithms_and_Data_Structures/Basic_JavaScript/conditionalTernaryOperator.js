c7e0557d726abeec57e66e7dce36eac3fcc6b6e8
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-10 18:51:37 +0100

    Use the Conditional (Ternary) Operator

---
+++ JavaScript_Algorithms_and_Data_Structures/Basic_JavaScript/conditionalTernaryOperator.js
@@ -0,0 +1,9 @@
+// Use the Conditional (Ternary) Operator
+
+function checkEqual(a, b) {
+    return a == b ? "Equal" : "Not Equal";
+  }
+  
+console.log(checkEqual(1, 2));
+console.log(checkEqual(1, 1));
+console.log(checkEqual(1, -1));
