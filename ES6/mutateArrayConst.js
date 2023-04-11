c46b89bcafb455b8628f8ba014c0e42e0c40b939
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-11 20:24:54 +0100

    Mutate an Array Declared with const

---
+++ ES6/mutateArrayConst.js
@@ -0,0 +1,13 @@
+// Mutate an Array Declared with const
+
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  "use strict";
+  // Using s = [2, 5, 7] would be invalid
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+  // Only change code above this line
+}
+console.log(editInPlace());
