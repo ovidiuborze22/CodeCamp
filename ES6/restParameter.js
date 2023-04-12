df19dde0c51acee5bd0d901ece2b47b7280fbfc2
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-12 12:42:57 +0100

    Use the Rest Parameter with Function Parameters

---
+++ ES6/restParameter.js
@@ -0,0 +1,14 @@
+// Use the Rest Parameter with Function Parameters
+
+const sum = (...args) => {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+      total += args[i];
+    }
+    return total;
+}
+
+console.log(sum(0,1,2));
+console.log(sum(1,2,3,4));
+console.log(sum(5));
+console.log(sum(0));
