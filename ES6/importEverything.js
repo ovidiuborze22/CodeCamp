da957e2f8e934bc5965f0886ed126bba998a5e43
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-14 15:04:31 +0100

    Use * to Import Everything from a File

--- ES6/importEverything.js
+++ ES6/importEverything.js
@@ -3,5 +3,5 @@ import * as stringFunctions from './exportCodeBlock.js';
 
 // Only change code above this line
 
-stringFunctions.uppercaseString("hello");
-stringFunctions.lowercaseString("WORLD!");
+console.log(stringFunctions.uppercaseString("hello"));
+console.log(stringFunctions.lowercaseString("WORLD!"));
