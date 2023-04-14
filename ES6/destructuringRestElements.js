09c12f1db458bfb4a9a3fd62fccf244cf30e870f
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-14 13:09:40 +0100

    Destructuring via rest elements

---
+++ ES6/destructuringRestElements.js
@@ -0,0 +1,14 @@
+// Destructuring via rest elements
+
+function removeFirstTwo(list) {
+    // Only change code below this line
+    //const [a,b, ...shorterList] = list; // Change this line
+    const [, , ...shorterList] = list; // Change this line
+    // Only change code above this line
+    return shorterList;
+  }
+  
+  const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+console.log(sourceWithoutFirstTwo);
