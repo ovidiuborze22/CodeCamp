073e6ede62e917cf26f754f9d51c10aec9952118
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-14 14:50:37 +0100

    Reuse JavaScript Code Using import

---
+++ ES6/importCodeReuse.js
@@ -0,0 +1,7 @@
+// Reuse JavaScript Code Using import
+
+import { uppercaseString, lowercaseString } from './exportCodeBlock';  
+// Only change code above this line
+
+console.log(uppercaseString("hello"));
+console.log(lowercaseString("WORLD!"));
