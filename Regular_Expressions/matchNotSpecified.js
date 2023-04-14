8bd210682b3c8f1fa5194434d96c9628f3525911
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-14 20:18:31 +0100

    Match Single Characters Not Specified

---
+++ Regular_Expressions/matchNotSpecified.js
@@ -0,0 +1,7 @@
+// Match Single Characters Not Specified
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou^0-9]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+
+console.log(result);
