d27483915db8711c7db1f0431423ae71e982023b
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-14 14:54:54 +0100

    Use * to Import Everything from a File

---
+++ ES6/importEverything.js
@@ -0,0 +1,7 @@
+// Use * to Import Everything from a File
+import * as stringFunctions from './exportCodeBlock.js';
+
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
