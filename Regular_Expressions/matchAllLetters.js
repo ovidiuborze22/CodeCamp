76a2a0536c85d89cb6751959652590b54656c679
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-15 12:36:17 +0100

    Match All Letters and Numbers

---
+++ Regular_Expressions/matchAllLetters.js
@@ -0,0 +1,7 @@
+// Match All Letters and Numbers
+
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/g; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
+
+console.log(result);
