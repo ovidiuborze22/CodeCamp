83acd183f712f48fc009239fe532efd15806cab6
Ovidiu Borze <ovidiuborze22@gmail.com>
2023-04-15 19:25:33 +0100

    Catch Mixed Usage of Single and Double Quotes

---
+++ JavaScript_Algorithms_and_Data_Structures/Debugging/catchQuotes.js
@@ -0,0 +1,4 @@
+// Catch Mixed Usage of Single and Double Quotes
+
+let innerHtml = "<p>Click here to <a href=\"#Home\">return home</a></p>"; // or using single quotes inside double quotes
+console.log(innerHtml);
